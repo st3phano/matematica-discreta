9f99ef751dfcccfc917c3ffd871f2d3b0eea9464
st3phano <31676683+st3phano@users.noreply.github.com>
2023-10-24 11:09:20 -0300

    excluir resoluções erradas

--- avaliacao03/08/d.py
+++
@@ -1,7 +0,0 @@
-def S(n):
-   if (n == 1):
-      return "p"
-   return S(n - 1) + " + q" + " * -1"
-
-for n in range(1, 7):
-   print(f"S({n}) = {S(n)}")
--- avaliacao03/11.py
+++
@@ -1,7 +0,0 @@
-Lista Rotina (Lista L, inteiro j) {
-	Se (j == 1)
-return L;
-	Encontre o L[i], o maior item da lista L entre 1 e j;
-	Troque o L[i] pelo item L[j];
-	return Rotina (L, j – 1);
-}
